a4b7a3d751aa14ce5ab341b2efa921aacc7cefd4
Felipe <fellipefreiire3@gmail.com>
2021-08-25 19:54:22 -0300

    new deploy test

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,17 +1,6 @@
-// import firebase from 'firebase/app'
-import firebase from 'firebase'
+import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
-
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-// appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-// }
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBRk6UkAyOMSvRpOn_XAi7cL7jsgdOk_60',
